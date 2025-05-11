f27dfeb6ca9bd5ed79c324b7f84b1bc3ad43f540
uluk <ulukraat@mail.ru>
2025-05-11 08:36:48 +0000

    0.2

--- src/main/java/com/example/Telegram/service/TelegramBot.java
+++ src/main/java/com/example/Telegram/service/TelegramBot.java
@@ -1,20 +1,33 @@
 package com.example.Telegram.service;
 
 import com.example.Telegram.config.BotConfig;
+import com.example.Telegram.model.Repository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@EnableScheduling
 @Component
 @Slf4j
 public class TelegramBot extends TelegramLongPollingBot {
+    final Repository repository;
     final BotConfig config;
+    private final Map<Long,Boolean> activeUsers = new ConcurrentHashMap<>();
+    private int currentWishIndex = 0;
 
-    private TelegramBot(BotConfig config) {
+    private TelegramBot(BotConfig config, Repository repository) {
         this.config = config;
+        this.repository = repository;
     }
 
 
@@ -28,22 +41,25 @@ public class TelegramBot extends TelegramLongPollingBot {
         return config.getToken();
     }
 
+
     @Override
     public void onUpdateReceived(Update update) {
         if(update.hasMessage() && update.getMessage().hasText()) {
             String message = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
+             long userChatId = update.getMessage().getChatId();
             switch(message){
                 case "/start":
-                    startCommandReceived(chatId,update.getMessage().getChat().getFirstName());
+                    activeUsers.put(userChatId,true);
+                    startCommandReceived(userChatId,update.getMessage().getChat().getFirstName());
                     break;
                 default:
-                    sendMessage(chatId,"fsdfds");
+                    sendMessage(userChatId,"\uD83D\uDCAB");
+                    log.info("Пользователем было отправлено [" + message + "]");
             }
         }
     }
     private void startCommandReceived(long chatId, String name) {
-        String answer = "Привет уцлуц";
+        String answer = "Привет Амира ,решил создать такого простого бота который будет тебе желать доброго утра и спокойной ночи,(каждый день желать в отличие от меня)\uD83D\uDC8C";
         log.info("Replied to user :" + name);
         sendMessage(chatId, answer);
 
@@ -58,4 +74,32 @@ public class TelegramBot extends TelegramLongPollingBot {
             log.error(e.getMessage());
         }
     }
+    @Scheduled(cron = "0 0 9 * * ?", zone = "Asia/Bishkek")
+    public void sendMorningWishes(){
+        List<String> mWishes = repository.getMorningWish();
+        if (mWishes.isEmpty()) {
+            log.info("В листе нет пожеланий");
+            return;
+        }
+        String toDayWish = mWishes.get(currentWishIndex);
+        for (Long userId : activeUsers.keySet()) {;
+            sendMessage(userId,toDayWish);
+            log.info("Пожелание отправлено");
+        }
+    }
+    @Scheduled(cron = "0 0 12 * * ?", zone = "Asia/Bishkek")
+    public void sendNightWishes(){
+        List<String> nWishes = repository.getNightWish();
+        if (nWishes.isEmpty()) {
+            log.info("В листе нет пожеланий");
+            return;
+        }
+        String toDayWish = nWishes.get(currentWishIndex);
+        for (Long userId : activeUsers.keySet()) {
+            sendMessage(userId,toDayWish);
+            log.info("Пожелание отправлено");
+        }
+        currentWishIndex = (currentWishIndex + 1) % nWishes.size();
+    }
+
 }
