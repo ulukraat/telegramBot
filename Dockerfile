c3e1924e6b3dd0a0b89613b4285dda4563852027
uluk <ulukraat@mail.ru>
2025-05-11 14:00:25 +0000

    0.4

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM openjdk:21-jdk-slim
 WORKDIR /app
 COPY target/*.jar app.jar
 EXPOSE 8080
-CMD ["java", "-jar", "app.jar"]
+CMD ["java", "-Dserver.address=0.0.0.0", "-Dserver.port=8080", "-jar", "app.jar"]
+
--- src/main/java/com/example/Telegram/Controller/WebhookController.java
+++ src/main/java/com/example/Telegram/Controller/WebhookController.java
@@ -1,22 +1,37 @@
-    package com.example.Telegram.Controller;
+package com.example.Telegram.Controller;
 
-    import com.example.Telegram.service.TelegramBot;
-    import org.springframework.http.ResponseEntity;
-    import org.springframework.web.bind.annotation.*;
-    import org.telegram.telegrambots.meta.api.objects.Update;
+import com.example.Telegram.service.TelegramBot;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-    @RestController
-    @RequestMapping("/webhook")
-    public class WebhookController {
-        private final TelegramBot telegramBot;
+@RestController
+@RequestMapping("/webhook")
+public class WebhookController {
+    private static final Logger logger = LoggerFactory.getLogger(WebhookController.class);
+    private final TelegramBot telegramBot;
 
-        public WebhookController(TelegramBot telegramBot) {
-            this.telegramBot = telegramBot;
-        }
-
-        @PostMapping
-        public ResponseEntity<?> onUpdateReceived(@RequestBody Update update) {
-            telegramBot.onWebhookUpdateReceived(update);
-            return ResponseEntity.ok("OK");
-        }
+    public WebhookController(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
     }
+
+    @PostMapping
+    public ResponseEntity<?> onUpdateReceived(@RequestBody Update update) {
+        logger.info("Получено обновление: {}", update);
+        telegramBot.onWebhookUpdateReceived(update);
+        return ResponseEntity.ok("OK");
+    }
+
+    @PostMapping("/webhook")
+    public ResponseEntity<String> onRawUpdateReceived(@RequestBody String updateJson) {
+        System.out.println("Получено обновление в JSON: " + updateJson);
+        return ResponseEntity.ok("OK");
+    }
+    @GetMapping("/")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Бот работает!");
+    }
+
+}
